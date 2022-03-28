4663ce05f9e4d3cf92d03ac61ab04b35c2488af2
faizi99 <faizan9april@gmail.com>
2022-03-28 13:01:34 +0500

    add my files

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from document_features.doc_features import *
+from text_comp.text_comp import *
 from key_value_cells.key_value_cell import *
 from hand_writing.breaking_and_classifying import *
 
 #get document features dataframe
-doc_dataframe=get_features()
+doc_dataframe=get_text_comp_features()
 print(doc_dataframe)
 
 #get page_wise key_value and cells dataframe
-key_val_cells_dataframe=get_key_cell_features()
-print(key_val_cells_dataframe)
+#key_val_cells_dataframe=get_key_cell_features()
+#print(key_val_cells_dataframe)
 
 #get page_wise hand_writing percentage
-hand_writing_percentage_dataframe=hand_written()
-print(hand_writing_percentage_dataframe)
+#hand_writing_percentage_dataframe=hand_written()
+#print(hand_writing_percentage_dataframe)
